86988029cb97e8cc3d641c05ad46dca74c563cce
larimae <larissamae60@gmail.com>
2024-10-07 16:59:42 -0700

    completed weatherServices and server files

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,45 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  const { city } = req.body;
+
+  try {
+    const weatherData = await WeatherService.getWeatherByCity(city);
+
   // TODO: save city to search history
+  await HistoryService.saveCityToHistory(city, weatherData);
+  res.json(weatherData);
+} catch (error) {
+  res.status(500).json({ error: 'Unable to retrieve weather data' });
+}
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (req: Request, res: Response) => {
+  try {
+    const history = await HistoryService.getSearchHistory();
+    res.json(history);
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to retrieve search history' });
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  
+  try {
+    await HistoryService.deleteCityById(id);
+    res.json({ message: 'City deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to delete city' });
+  }
+});
 
 export default router;
--- Develop/server/src/service/weatherService.ts
+++ Develop/server/src/service/weatherService.ts
@@ -15,10 +15,6 @@ interface Coordinates {
 
 
 // TODO: Complete the WeatherService class
-class WeatherService {
-  private baseUrl: string;
-
-}
   // TODO: Define the baseURL, API key, and city name properties
   // TODO: Create fetchLocationData method
   // private async fetchLocationData(query: string) {}
@@ -33,28 +29,8 @@ class WeatherService {
   // TODO: Create fetchWeatherData method
   // private async fetchWeatherData(coordinates: Coordinates) {}
   // TODO: Build parseCurrentWeather method
-  private parseCurrentWeather(response: any): Weather {
-    const weather = response.list[0];
-
-    const id = weather[0].weather.id;
-    const main = weather[0].weather.main;
-    const description = weather[0].weather.description;
-    const icon = weather[0].weather.icon;
-    const city = response.city.name;
-  }
   // private parseCurrentWeather(response: any) {}
   // TODO: Complete buildForecastArray method
-  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
-    const forecastArray: weather[] = [];
-
-    for (const data of weatherData) {
-      const id = data.id;
-      const main = data.main;
-      const description = data.description;
-      const icon = data.icon;
-      const city = data.name;
-    }
-  }
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
   // async getWeatherForCity(city: string) {}
